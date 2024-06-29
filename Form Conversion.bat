bd1331f0e5e0945775ee37a5f90528887801a706
jspowley <jpowley@ualberta.ca>
2024-06-29 15:36:31 -0600

    Integration | Process Improvement

---
+++ Form Conversion.bat
@@ -0,0 +1,9 @@
+@echo off
+:: Get the directory of the batch file itself
+set script_dir=%~dp0
+
+:: Change to the Source directory (one level down)
+cd /d "%script_dir%Source"
+
+:: Run the R script with Rscript.exe
+"C:\Program Files\R\R-4.3.2\bin\Rscript.exe" "Script 4.0.R"
